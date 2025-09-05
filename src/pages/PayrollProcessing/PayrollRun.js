883f79b284d7779f7173c2fea436eadf5de4fc4f
Ian Rey Zafra <zafraayanrey@gmail.com>
2025-09-05 16:04:57 +0800

    v

--- src/pages/PayrollProcessing/PayrollRun.js
+++ src/pages/PayrollProcessing/PayrollRun.js
@@ -1,7 +1,63 @@
-import React from "react";
+import * as React from "react";
+import { useForm, Controller } from "react-hook-form";
+import { TextField, Button } from "@mui/material";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
-function PayrollRun() {
-  return <div>PayrollRun</div>;
+export default function PayrollRun() {
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      payrollDate: null,
+    },
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="periodStart"
+          control={control}
+          render={({ field, fieldState }) => (
+            <DatePicker
+              {...field}
+              label="Payroll Date"
+              slotProps={{
+                textField: {
+                  fullWidth: true,
+                  error: !!fieldState.error,
+                  helperText: fieldState.error?.message,
+                },
+              }}
+            />
+          )}
+        />
+        <Controller
+          name="periodEnd"
+          control={control}
+          render={({ field, fieldState }) => (
+            <DatePicker
+              {...field}
+              label="Payroll Date"
+              slotProps={{
+                textField: {
+                  fullWidth: true,
+                  error: !!fieldState.error,
+                  helperText: fieldState.error?.message,
+                },
+              }}
+            />
+          )}
+        />
+
+        <Button type="submit" variant="contained" sx={{ mt: 2 }}>
+          Submit
+        </Button>
+      </form>
+    </LocalizationProvider>
+  );
 }
-
-export default PayrollRun;
